fd8efca53e8350116023c2a86d26827bb93b0d45
qianyugang <yugang.qian@billionsfinance.cn>
2015-11-11 09:45:53 +0800

    添加commend文件

---
+++ app/Commands/GetZhihuBookMark.php
@@ -0,0 +1,31 @@
+<?php namespace App\Commands;
+
+use App\Commands\Command;
+
+use Illuminate\Contracts\Bus\SelfHandling;
+
+class GetZhihuBookMark extends Command implements SelfHandling {
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		//
+	}
+
+	/**
+	 * Execute the command.
+	 *
+	 * @return void
+	 */
+	public function handle()
+	{
+		//
+
+		echo 1;die;
+	}
+
+}
